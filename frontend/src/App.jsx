3a75e0560c2e3cf597edb6a0e86d86ecaa9e4a39
yours-majesty <Rjp#1234>
2024-10-19 13:20:20 +0530

    project complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom"; // Fixed imports
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom"; 
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar";
 import AddBank from "./pages/AddBank";
-import BankAccounts from "./pages/BankAccounts"; // Fixed typo: BankAccounts
-import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
+import BankAccount from "./pages/BankAccount"; 
+import { AuthProvider } from "./context/AuthContext"; 
 
 function App() {
   return (
     <>
-      <AuthProvider> {/* Wrap your app with AuthProvider */}
+      <AuthProvider> 
         <Router>
           <Navbar />
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signUp" element={<SignUp />} />
             <Route path="/addAccount" element={<AddBank />} />
-            <Route path="/bankAccounts" element={<BankAccounts />} />
+            <Route path="/bankAccounts" element={<BankAccount />} />
           </Routes>
         </Router>
         <ToastContainer />
